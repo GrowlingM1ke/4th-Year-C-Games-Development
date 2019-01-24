57244cfae437c41e855774d7c236e9b2e94f2b59
Lange <40212673@live.napier.ac.uk>
2019-01-24 12:03:24 +0000

    Generated invaders in space invaders

--- space_invaders/main.cpp
+++ space_invaders/main.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "ship.h"
 
 using namespace sf;
 using namespace std;
@@ -9,25 +10,47 @@ const int gameWidth = 800;
 const int gameHeight = 600;
 
 Texture spritesheet;
-Sprite invader;
+std::vector<Ship *> ships;
+
+void Update(float &dt) {
+	for (auto &s : ships) {
+		s->Update(dt);
+	}
+}
 
 void Load() {
 	if (!spritesheet.loadFromFile("res/img/invaders_sheet.png")) {
 		cerr << "Failed to load spritesheet!" << endl;
 	}
-	invader.setTexture(spritesheet);
-	invader.setTextureRect(IntRect(0, 0, 32, 32));
+
+	for (int r = 0; r < 5; ++r) {
+		auto rect = IntRect(0, 0, 32, 32);
+		for (int c = 0; c < 12; ++c) {
+			Vector2f position = { 60.0f * (c + 1), 50.0f * (r + 1)};
+			auto inv = new Invader(rect, position);
+			ships.push_back(inv);
+		}
+	}
+	Invader::speed = 20;
+
+	auto def = new Player();
+	ships.push_back(def);
 }
 
 void Render(RenderWindow &window) {
-	window.draw(invader);
+	for (const auto s : ships) {
+		window.draw(*s);
+	}
 }
 
 int main() {
 	RenderWindow window(VideoMode(gameWidth, gameHeight), "SPACE INVADERS");
 	Load();
+	static sf::Clock clock;
 	while (window.isOpen()) {
+		float dt = clock.restart().asSeconds();
 		window.clear();
+		Update(dt);
 		Render(window);
 		window.display();
 	}
---
+++ space_invaders/ship.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Ship : public sf::Sprite {
+protected:
+	sf::IntRect _sprite;
+	Ship();
+
+public:
+	explicit Ship(sf::IntRect ir);
+	virtual ~Ship() = 0;
+	virtual void Update(const float &dt);
+};
+
+class Invader : public Ship {
+public:
+	Invader(sf::IntRect ir, sf::Vector2f pos);
+	Invader();
+	void Update(const float &dt) override;
+	static bool direction;
+	static float speed;
+};
+
+class Player : public Ship {
+public:
+	Player();
+	void Update(const float &dt) override;
+};
