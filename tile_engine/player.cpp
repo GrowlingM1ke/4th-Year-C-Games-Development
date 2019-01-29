f23c522309b27cafce7f3d097f9e7e5c82571dc5
GrowlingM1ke <michallange1995@gmail.com>
2019-01-29 22:26:43 +0000

    Setup  the fundamentals of the tile_engine

---
+++ tile_engine/player.cpp
@@ -0,0 +1,34 @@
+#include "player.h"
+using namespace sf;
+using namespace std;
+
+
+const Keyboard::Key controls[4] = {
+	Keyboard::Right,
+	Keyboard::Left,
+	Keyboard::Up,
+	Keyboard::Down
+};
+
+void Player::update(double dt) {
+	//Move in four directions based on keys
+	if (Keyboard::isKeyPressed(controls[0]))
+		move(Vector2f(10.0f * dt, 0.0f));
+	if (Keyboard::isKeyPressed(controls[1]))
+		move(Vector2f(-10.0f * dt, 0.0f));
+	if (Keyboard::isKeyPressed(controls[0]))
+		move(Vector2f(0.0f, 10.0f * dt));
+	if (Keyboard::isKeyPressed(controls[1]))
+		move(Vector2f(0.0f, -10.0f * dt));
+
+	Entity::update(dt);
+}
+
+Player::Player() : _speed(200.0f), Entity(make_unique<CircleShape>(25.f)) {
+	_shape->setFillColor(Color::Magenta);
+	_shape->setOrigin(Vector2f(25.f, 25.f));
+}
+
+void Player::render(sf::RenderWindow &window) const {
+	window.draw(*_shape);
+}
