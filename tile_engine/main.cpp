f040f88a1a02034ef05545c2f94bce792dc07620
GrowlingM1ke <michallange1995@gmail.com>
2019-01-29 22:44:56 +0000

    added assets folder and cleaned up main.cpp

--- tile_engine/main.cpp
+++ tile_engine/main.cpp
@@ -1,7 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
-#include "ship.h"
-#include "bullet.h"
+#include "entity.h"
+#include "player.h"
 
 using namespace sf;
 using namespace std;
@@ -10,76 +10,28 @@ using namespace std;
 const int gameWidth = 800;
 const int gameHeight = 600;
 
-Texture spritesheet;
-std::vector<Ship *> ships;
-
 void Update(float &dt, RenderWindow &window) {
-	for (auto &s : ships) {
-		s->Update(dt);
-	}
 
 	// Quit Via ESC Key
 	if (Keyboard::isKeyPressed(Keyboard::Escape)) {
 		window.close();
 	}
-
-	Bullet::Update(dt);
 }
 
 void Load() {
-	if (!spritesheet.loadFromFile("res/img/invaders_sheet.png")) {
-		cerr << "Failed to load spritesheet!" << endl;
-	}
 
-	for (int r = 0; r < 5; ++r) {
-		auto rect = IntRect(0, 0, 32, 32);
-		for (int c = 0; c < 12; ++c) {
-			Vector2f position = { 60.0f * (c + 1), 50.0f * (r + 1)};
-			auto inv = new Invader(rect, position);
-			ships.push_back(inv);
-		}
-	}
-	Invader::speed = 20;
-
-	auto def = new Player();
-	ships.push_back(def);
-}
-
-void ReLoad() {
-	for (auto *s : ships) {
-		delete s;
-	}
-	ships.clear();
-
-	for (int r = 0; r < 5; ++r) {
-		auto rect = IntRect(0, 0, 32, 32);
-		for (int c = 0; c < 12; ++c) {
-			Vector2f position = { 60.0f * (c + 1), 50.0f * (r + 1) };
-			auto inv = new Invader(rect, position);
-			ships.push_back(inv);
-		}
-	}
-
-	auto def = new Player();
-	ships.push_back(def);
 }
 
 void Render(RenderWindow &window) {
-	for (const auto s : ships) {
-		window.draw(*s);
-	}
-	Bullet::Render(window);
+	
 }
 
 int main() {
-	RenderWindow window(VideoMode(gameWidth, gameHeight), "SPACE INVADERS");
+	RenderWindow window(VideoMode(gameWidth, gameHeight), "TILE ENGINE");
 	Load();
 	static sf::Clock clock;
 	while (window.isOpen()) {
 		float dt = clock.restart().asSeconds();
-		if (Keyboard::isKeyPressed(Keyboard::R)) {
-			ReLoad();
-		}
 		window.clear();
 		Update(dt, window);
 		Render(window);
